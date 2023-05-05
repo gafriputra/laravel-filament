f47009b813636b0515c8528efdfa05457de8218c
gafriputra <gafri.putra@gmail.com>
2023-05-05 08:58:55 +0700

    artisan

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers;
 
 use PDF;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class PdfController extends Controller
 {
-    public function index() 
+    public function index()
     {
         $pdf = PDF::loadView('pdf.sample', [
             'logo'    => public_path('img/logo.PNG'),
         ]);
 
         return $pdf->download('hotel-voucher.pdf');
+    }
+
+    public function artisan()
+    {
+        Artisan::call();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('pdf', [PdfController::class, 'index']);
+Route::get('artisan', [PdfController::class, 'artisan']);
